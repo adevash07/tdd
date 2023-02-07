10edafe6eb3140312ee2643a8b424195af3dd3a7
debugger <58630922+adevash07@users.noreply.github.com>
2023-02-07 04:36:47 +0100

    first convertRoman draft

--- src/romanNumeral.js
+++ src/romanNumeral.js
@@ -1,3 +1,22 @@
 export const convertToRomanFigure = (number) => {
-  return "I";
+  //let's say our number is 7
+  const romanBreakPoints = [1, 5, 10];
+  const romanPairs = {
+    1: "I",
+    5: "V",
+    10: "X",
+  };
+
+  let answer;
+
+  //loop to our breakpoint and upper breakpoint, then upper breakpoint will be 10 and base will be 5
+  for (let i = 0; i < romanBreakPoints.length; i++) {
+    let base;
+    let remainder;
+    if (romanBreakPoints[i] > number) {
+      base = romanBreakPoints[i - 1];
+      answer += romanPairs[base];
+      remainder = number - romanPairs[base]; //2
+    }
+  }
 };
--- src/romanNumeral.test.js
+++ src/romanNumeral.test.js
@@ -3,5 +3,6 @@ import { convertToRomanFigure } from "./romanNumeral";
 describe("convert numeric numbers to roman figures", () => {
   test("convert number 1 to Roman figure 1", () => {
     expect(convertToRomanFigure(1)).toBe("I");
+    expect(convertToRomanFigure(7)).toBe("VII");
   });
 });
